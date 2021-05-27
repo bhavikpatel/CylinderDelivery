4f5ec9091b342a67429e49c4559d04eec09072ff
bhavik777 <bhavikkumar.m@gmail.com>
2021-05-27 15:16:55 +0530

    Deliverynote module

--- app/src/main/java/com/track/cylinderdelivery/ui/diliverynote/DeliveryNoteFragment.java
+++ app/src/main/java/com/track/cylinderdelivery/ui/diliverynote/DeliveryNoteFragment.java
@@ -1,13 +1,19 @@
 package com.track.cylinderdelivery.ui.diliverynote;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,23 +23,46 @@ import com.track.cylinderdelivery.ui.product.AddProductActivity;
 
 public class DeliveryNoteFragment extends Fragment {
 
+    Context context;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_delivery_note, container, false);
+        context=getActivity();
+        setHasOptionsMenu(true);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.Delivery_Note_List
+        ));
        // final TextView textView = root.findViewById(R.id.text_home);
        // final Button button=root.findViewById(R.id.button);
-        Button btnAddProduct=root.findViewById(R.id.btnAddProduct);
-        RecyclerView recyclerView=root.findViewById(R.id.rv_product_list);
+        //Button btnAddProduct=root.findViewById(R.id.btnAddProduct);
+        RecyclerView recyclerView=root.findViewById(R.id.rv_delivery_list);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
 
-        btnAddProduct.setOnClickListener(new View.OnClickListener() {
+/*        btnAddProduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(getActivity(), AddProductActivity.class);
                 getActivity().startActivity(intent);
             }
-        });
+        });*/
         return root;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.deliverynote_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_addDiliverNote:
+                // do stuff, like showing settings fragment
+                Intent intent=new Intent(context,AddDeliveryNoteActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
